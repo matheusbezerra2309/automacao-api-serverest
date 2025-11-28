6c89e53dc671f115634390bccd1c8dddbd88914b
Seu Nome <seuemail@exemplo.com>
2025-11-28 12:02:00 -0300

    Relatórios HMTL

--- cypress.config.js
+++ cypress.config.js
@@ -3,15 +3,23 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://serverest.dev',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',  
-    supportFile: false, 
-    setupNodeEvents(on, config) {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+    setupNodeEvents(on, config) {},
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/mochawesome',
+      overwrite: false,
+      html: false,
+      json: true,
+      timestamp: 'isoDateTime',
+      quiet: true
     },
   },
   env: {
     email: 'fulano@qa.com',
     password: 'teste',
     novoUsuarioId: null,
-    
+
   }
 })
